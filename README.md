51459cf8c96b1a843293c6b6ba5d61466d2faea6
alperen <zahid.alperen@linspark.com>
2022-01-29 02:52:07 +0300

    Test Structure Added

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # react-graphql-node
+
+For testing we can add Enzyme to test components.
+
+
+## Tech Stack
+### Jest and Enzyme for Testing
+Jest is mainly used for assertion, while Enzyme deals
+with testing react component like render components, 
+and checking things like props and states.
+
+My personal choice is to place test files 
+in the component folder itself.
+
+https://github.com/enzymejs/enzyme-matchers/tree/master/packages/jest-enzyme
---
+++ frontend-sample/src/App.spec.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from './App';
+//import {render, cleanup} from '@testing-library/react'
+
+
+
+//This basically describes the test. It requires two parameters, like the name of the test and a function that holds the entire test.
+describe("App Component", function() {
+    it("should render the App Component", function() {
+        const container = shallow(<App />);
+        const de = container.find(`div`);
+        //Condition needed to pass the test. Expect condition will compare the received parameter to a matcher.
+        expect(de.length).toEqual(1);
+    });
+
+    //Testing with testing-library/react
+    // afterEach(cleanup)
+    // test('should generate a snapshot for app component', () => {
+    //     const { asFragment } = render(<App />)
+    //     expect(asFragment()).toMatchSnapshot()
+    // })
+});
+
+// export default describe('Addition', () => {
+//     it('should know 5 + 4 equals 9 ', () => {
+//         expect(5 + 4).toBe(9);
+//     });
+// });
